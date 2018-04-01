262327c8227af8a98fec5b4adc6a03059a14a304
vergelbarit35@gmail.com <vergelbarit35@gmail.com>
2018-04-01 20:12:16 +0800

    clean up

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var mathLib = require('./lib/math');
-var jokesLib = require('./lib/jokes');
+const mathLib = require('./lib/math'),
+    environment = require('./config'),
+    jokesLib = require('./lib/jokes');
 
 //App object
-var app = {};
+let app = {};
 
 app.config = {
     'timeBetweenJokes': 1000
@@ -10,26 +11,26 @@ app.config = {
 
 
 //function that prints a random joke
-app.printAjoke = function() {
+app.printAjoke = () => {
 
     //Get all the jokes
-    var allJokes = jokesLib.allJokes();
+    let allJokes = jokesLib.allJokes();
 
     // Get the length of the jokes
-    var numberOfJokes = allJokes.length;
+    let numberOfJokes = allJokes.length;
 
     //Pick a random number between 1 and the number of jokes
-    var randomNumber = mathLib.getRandomNumber(1, numberOfJokes);
+    let randomNumber = mathLib.getRandomNumber(1, numberOfJokes);
 
     //get the joke at that position in the array(minus one)
-    var selectedJoke = allJokes[randomNumber - 1];
+    let selectedJoke = allJokes[randomNumber - 1];
 
     // Send the Joke to the console
     console.log(selectedJoke);
 };
 
 //Function that loops indefinitely, calling the printAjoke function as it goes
-app.indefiniteLoop = function() {
+app.indefiniteLoop = () => {
     // create the interval, using the config variable defined above
     setInterval(app.printAjoke, app.config.timeBetweenJokes);
 };
